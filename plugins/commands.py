52fc24cabcd46144b7323180beab623cc197a9ae
Movie Show <105733634+skdrono05@users.noreply.github.com>
2022-05-23 10:56:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,11 +44,11 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/Drono_Bot_Updates')
+            InlineKeyboardButton('💥𝙾𝚆𝙽𝙴𝚁💥',url='https://t.me/drono05')),
+            InlineKeyboardButton('🤖𝚄𝙿𝙳𝙰𝚃𝙴𝚂🤖',url='https://t.me/Drono_Bot_Updates')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿 ℹ️', callback_data='help'),
+            InlineKeyboardButton('💫 𝙰𝙱𝙾𝚄𝚃 💫', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -90,11 +90,11 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/Drono_Bot_Updates')
+            InlineKeyboardButton('💥𝙾𝚆𝙽𝙴𝚁💥',url='https://t.me/drono05')
+            InlineKeyboardButton('🤖𝚄𝙿𝙳𝙰𝚃𝙴𝚂🤖',url='https://t.me/Drono_Bot_Updates')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿 ℹ️', callback_data='help'),
+            InlineKeyboardButton('💫 𝙰𝙱𝙾𝚄𝚃 💫', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
